e15359934d9f04a72ee590267b7e8eb282524c61
notoriginallink <toldan04@yandex.ru>
2024-04-18 18:35:40 +0300

    chore: rename dto classes

--- kotiki-app/service/src/main/java/ru/tolstov/services/dto/OwnerDto.java
+++ kotiki-app/service/src/main/java/ru/tolstov/services/dto/OwnerDto.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class OwnerItem {
+public class OwnerDto {
     String firstName;
     String lastName;
     LocalDate birthdate;
     List<Long> catIds;
 
-    public OwnerItem(Owner owner) {
+    public OwnerDto(Owner owner) {
         this.firstName = owner.getFirstName();
         this.lastName = owner.getLastName();
         this.birthdate = owner.getBirthdate();
