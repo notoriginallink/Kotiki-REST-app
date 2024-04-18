20fbbb8094b7c49a9b343217985022a6832a9faa
notoriginallink <toldan04@yandex.ru>
2024-04-18 16:44:20 +0300

    chore: clear unused imports

--- kotiki-app/src/main/java/ru/tolstov/Main.java
+++ kotiki-app/src/main/java/ru/tolstov/Main.java
@@ -1,15 +1,7 @@
 package ru.tolstov;
 
-import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.tolstov.models.CatColor;
-import ru.tolstov.repositories.LocalCatRepository;
-import ru.tolstov.repositories.LocalOwnerRepository;
-import ru.tolstov.services.CatServiceImpl;
-import ru.tolstov.services.OwnerServiceImpl;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Main {
